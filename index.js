4a66f305375d2368c279751021481cfd9174a2f4
Sagar Parmar <sagarparmar0233@gmail.com>
2023-02-09 10:07:57 -0600

    added server with express

---
+++ index.js
@@ -0,0 +1,45 @@
+// AN Express Server
+
+const openAI = require("openai");
+
+const { Configuration, OpenAIApi } = openAI;
+
+const configuration = new Configuration({
+  organization: "org-MtlRfgywWJev3QusIu95FI85",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Pretend you are Elon Musk. Answet with motivational content. 
+    Musk: How can I help you today?
+    Person: I want some motivation.
+    Musk: You are amazing, You push the bounries of technology.
+    Person: ${message}?
+    Musk:`,
+    max_tokens: 100,
+    temperature: 0,
+  });
+  console.log(response.data);
+  if (response.data) {
+    if (response.data.choices) {
+      res.json({ message: response.data.choices[0].text });
+    }
+  }
+});
+
+app.listen(port, () => {
+  console.log("Example app listening");
+});
